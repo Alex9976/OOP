52506f2ebbff80a7eae6cebd3d6fcf8a6fffe016
Alexander <60441130+Alex9976@users.noreply.github.com>
2021-02-17 23:09:08 +0300

    Update Program.cs
    Some fixies

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ namespace OOPLab1
         {
 
             Transport[] transport = new Transport[] { new Car("Audi"), new Bus("MAN"), new Airplane("Airbus"), new Helicopter("Robinson"), new Sea("Tornado") };
-
-
             for (int i = 0; i < transport.Length; i++)
             {
                 transport[i].AskInfo();
